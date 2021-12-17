1494ee5a7ef3fab3fb6a84698afab55086d6d201
Tim <tsimms@gmail.com>
2021-12-17 13:48:10 -0600

    add read_file.c

--- read_file.c
+++ read_file.c
@@ -2,38 +2,30 @@
 
 /**
  * read_file - parses the monty file
- * @fp: pointer to file
- *
+ * @stack: It's the stack
  * Return: void
  *
  */
 
-void read_file(FILE *fp)
+void read_file(stack_t **stack)
 {
 
-	/* unsigned int line_num = 1; */
+	unsigned int line_num = 1;
 	char *line = NULL;
 	char *execute;
-	char **words;
 	ssize_t read_line;
 	size_t length = 0;
 
-	while ((read_line = getline(&line, &length, fp)) != -1)
+	while ((read_line = getline(&line, &length, efs.fp)) != -1)
 	{
-		execute = strtok(line, "\n");
-		if (execute != NULL)
-		{
-		/*	printf("%s\n", execute); */
-			words = ndsplitter(execute, " ");
-			if (words != NULL)
-			{
-				printf("%c\n", *words[1]);
-			}
-		free_list(words);
-		}
+		execute = strtok(line, DELIMS);
 
+		efs.line = line;
+		if (execute)
+			check_command(stack, execute, line_num);
+
+
+		line_num++;
 	}
-	if (line)
-		free(line);
-}
 
+}
